ecc68d6adadafce96fcdd9a45bd9195fa8893c8c
David Chmura <dschmura@humbledaisy.com>
2016-11-23 10:54:51 -0500

    Add cosign and redirect_https to applicaiton controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :redirect_https, :cosign_uniqname
+  
+  private
+  def cosign_uniqname
+    @cosign_uniqname = request.headers.env.fetch('REMOTE_USER')
+  end
+
+  def redirect_https
+    @ip = request.remote_ip
+    redirect_to protocol: "https://" unless (request.ssl? || @ip.match(/127.0.0.1|::1/))
+  end
 end
